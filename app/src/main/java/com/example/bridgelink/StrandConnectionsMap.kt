ab5aeb671fb3bb03dbbc3e3be2c29e6b02a90280
Martim Emauz <martim.emauz@gmail.com>
2025-01-02 18:23:54 +0000

    Utility Bar on strandconnections

--- app/src/main/java/com/example/bridgelink/StrandConnectionsMap.kt
+++ app/src/main/java/com/example/bridgelink/StrandConnectionsMap.kt
@@ -1,31 +1,34 @@
 package com.example.bridgelink
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.bridgelink.navigation.NavigationDots
-import com.example.bridgelink.navigation.Screens
 import com.mapbox.maps.extension.compose.MapEffect
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
 import com.mapbox.maps.extension.compose.style.MapStyle
 import com.mapbox.maps.plugin.PuckBearing
+import com.mapbox.maps.plugin.compass.compass
 import com.mapbox.maps.plugin.locationcomponent.createDefault2DPuck
 import com.mapbox.maps.plugin.locationcomponent.location
 
@@ -38,6 +41,7 @@ fun StrandConnectionsMap (navController: NavController, modifier: Modifier = Mod
             .fillMaxSize()
             .background(colorResource(id = R.color.navy_blue))
     ) {
+        NavigationUtils()
         Box(
             modifier = Modifier
                 .weight(1f)
@@ -60,5 +64,85 @@ fun StrandConnectionsMap (navController: NavController, modifier: Modifier = Mod
             }
         }
         NavigationDots(currentPage = 3)
+    }
+}
+
+@Composable
+fun NavigationUtils() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(64.dp)
+            .background(colorResource(id = R.color.navy_blue)),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // Cargo Status
+        Row(
+            modifier = Modifier
+                .weight(1f),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.box_emoji),
+                contentDescription = null,
+                tint = Color.Unspecified, // Para a imagem não ficar a preto
+                modifier = Modifier
+                    .size(42.dp)
+                    .padding(end = 4.dp)
+            )
+            Text(
+                text = "89%",
+                color = Color.White,
+                fontSize = 24.sp
+            )
+        }
+
+        // ETA
+        Column (
+            modifier = Modifier
+                .weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "1h05min",
+                    color = Color.White,
+                    fontSize = 24.sp
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "ETA",
+                    color = Color.LightGray,
+                    fontSize = 12.sp
+                )
+            }
+        }
+
+        // Distance left
+        Row(
+            modifier = Modifier
+                .weight(1f),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "5.75km",
+                color = Color.White,
+                fontSize = 24.sp
+            )
+        }
     }
 }
--- app/src/main/java/com/example/bridgelink/navigation/NavGraph.kt
+++ app/src/main/java/com/example/bridgelink/navigation/NavGraph.kt
@@ -1,39 +1,42 @@
 package com.example.bridgelink.navigation
 
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-
 //importing the screens
-import com.example.bridgelink.CargoManagement
-import com.example.bridgelink.ChiralNetworkMap
-import com.example.bridgelink.Menu
-import com.example.bridgelink.Profile
-import com.example.bridgelink.ShipmentOptimizationMap
-import com.example.bridgelink.StrandConnectionsMap
-import com.example.bridgelink.TimeFallForecast
-import com.example.bridgelink.OdradekScanner
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.bridgelink.CargoManagement
+import com.example.bridgelink.ChiralNetworkMap
+import com.example.bridgelink.Menu
+import com.example.bridgelink.OdradekScanner
+import com.example.bridgelink.Profile
 import com.example.bridgelink.R
-
+import com.example.bridgelink.ShipmentOptimizationMap
+import com.example.bridgelink.StrandConnectionsMap
+import com.example.bridgelink.TimeFallForecast
 
 
 @Composable
 fun NavGraph(navController: NavHostController, modifier: Modifier = Modifier) {
     NavHost(
         navController = navController,
-        startDestination = Screens.StrandConnectionsMap.route,
+        startDestination = Screens.StrandConnectionsMap.route, // StrandConnectionsMap ou Menu
         modifier = modifier
     ) {
         composable(route = Screens.Menu.route) {
@@ -69,19 +72,23 @@ fun NavigationDots(currentPage: Int) {
         modifier = Modifier
             .fillMaxWidth()
             .background(colorResource(id = R.color.navy_blue))
-            .padding(vertical = 16.dp),
+            .padding(vertical = 12.dp),
         horizontalArrangement = Arrangement.Center
     ) {
         repeat(8) { index ->
             Box(
                 modifier = Modifier
-                    .padding(horizontal = 4.dp)
-                    .size(16.dp)
+                    .padding(horizontal = 6.dp)
+                    .size(
+                        if (currentPage == index) 50.dp
+                        else 40.dp
+                    )
                     .clip(CircleShape)
                     .background(
                         if (currentPage == index) Color(0xFFFF5722)
                         else Color.Gray
                     )
+                    .align(Alignment.CenterVertically)
             )
         }
     }
